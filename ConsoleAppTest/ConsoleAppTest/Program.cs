b5f4ca7eeb2c569bbaf095171d53fb8985e65c66
DeN2828 <den-tu@ua.fm>
2022-10-09 17:40:12 +0300

    Version 1.0.0.1

---
+++ ConsoleAppTest/ConsoleAppTest/Program.cs
@@ -0,0 +1,5 @@
+﻿// Версия 1.0.0.1
+Console.WriteLine("Enter meter: ");
+Console.WriteLine($"{int.Parse(Console.ReadLine()) * 100} centimeter");
+Console.WriteLine("Enter meter: ");
+Console.WriteLine($"{float.Parse(Console.ReadLine()) / 1000} kilometers");
