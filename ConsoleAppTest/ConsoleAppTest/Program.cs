af547d99867f22bf58410297815041b2d6907cfe
DeN2828 <den-tu@ua.fm>
2022-10-09 17:42:53 +0300

    Version 1.0.0.2

--- ConsoleAppTest/ConsoleAppTest/Program.cs
+++ ConsoleAppTest/ConsoleAppTest/Program.cs
@@ -1,5 +1,13 @@
+<<<<<<< Updated upstream
 ﻿// Версия 1.0.0.1
 Console.WriteLine("Enter meter: ");
 Console.WriteLine($"{int.Parse(Console.ReadLine()) * 100} centimeter");
 Console.WriteLine("Enter meter: ");
 Console.WriteLine($"{float.Parse(Console.ReadLine()) / 1000} kilometers");
+=======
+﻿// Версия 1.0.0.2
+Console.WriteLine("Enter gramm: ");
+Console.WriteLine($"{float.Parse(Console.ReadLine()) / 1000} liters");
+Console.WriteLine("Enter liters: ");
+Console.WriteLine($"{float.Parse(Console.ReadLine()) / 3,785} gallons");
+>>>>>>> Stashed changes
