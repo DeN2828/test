93d5211bb1ff0dfad6969261dcd85b32d60fb942
DeN2828 <den-tu@ua.fm>
2022-10-09 17:44:34 +0300

    Version 1.0.0.3

--- ConsoleAppTest/ConsoleAppTest/Program.cs
+++ ConsoleAppTest/ConsoleAppTest/Program.cs
@@ -1,4 +1,5 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 ï»¿// Ğ’ĞµÑ€ÑĞ¸Ñ 1.0.0.1
 Console.WriteLine("Enter meter: ");
 Console.WriteLine($"{int.Parse(Console.ReadLine()) * 100} centimeter");
@@ -10,4 +11,9 @@ Console.WriteLine("Enter gramm: ");
 Console.WriteLine($"{float.Parse(Console.ReadLine()) / 1000} liters");
 Console.WriteLine("Enter liters: ");
 Console.WriteLine($"{float.Parse(Console.ReadLine()) / 3,785} gallons");
+>>>>>>> Stashed changes
+=======
+// Âåğñèÿ 1.0.0.3
+Console.WriteLine("Enter celsia: ");
+Console.WriteLine($"{float.Parse(Console.ReadLine()) * (9 / 5) + 32} Farengeita");
 >>>>>>> Stashed changes
